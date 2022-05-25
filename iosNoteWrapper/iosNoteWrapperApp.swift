fd5aa5da811dc38ff19548bf40a1ac86ec3b268a
MY <715711877@qq.com>
2022-05-25 16:29:48 +0800

    Initial Commit

---
+++ iosNoteWrapper/iosNoteWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosNoteWrapperApp.swift
+//  iosNoteWrapper
+//
+//  Created by mymac on 2022/5/25.
+//
+
+import SwiftUI
+
+@main
+struct iosNoteWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
